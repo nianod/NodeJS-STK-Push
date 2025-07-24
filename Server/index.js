a216d5137322aaaaa7cc998c1e526cf971393c0f
nianod <arnoldkk422@gmail.com>
2025-07-24 23:19:23 +0300

    Remove node_modules from Git tracking

---
+++ Server/index.js
@@ -0,0 +1,7 @@
+const axios = require('axios')
+const config = require('/Config')
+
+const getAccessToken = async() => {
+    const auth = Buffer.from(${config.consumerKey}: 
+        ${config.consumerSecret}).toString('base64');
+}
