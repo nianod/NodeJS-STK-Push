f2cedb31ee3dbd699dc5562bde8b79d306a7f9d7
nianod <arnoldkk422@gmail.com>
2025-07-25 00:24:38 +0300

    stk

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,20 @@
 const axios = require('axios')
-const config = require('/Config')
+const config = require('/Config');
+const { response } = require('express');
 
 const getAccessToken = async() => {
     const auth = Buffer.from(${config.consumerKey}: 
         ${config.consumerSecret}).toString('base64');
+
+    const response = await axios.get('https:                                                                             
+      headers: {
+      '//sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
+      headers: {
+       'Authorization': Basic ${auth},
+      },
+    })
+  return response.data.access_token;
+  
 }
+
+module.exports = getAccessToken
