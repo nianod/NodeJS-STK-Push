3cae472553b0c8f252cb9deaffd701ac659b7e51
nianod <arnoldkk422@gmail.com>
2025-07-25 19:59:38 +0300

    removed node modules

--- Server/config.js
+++ Server/config.js
@@ -1,7 +1,8 @@
-export const config = {
+const config = {
     consumerKey : "kkcDPh2f7Ev5guUzgUtGfAQZ77atXMG9b0b1jGUUzmIY01Gk",
     consumerSecret : "O5shZa1XB2d55XMbJRYnJdsvwtH2XqPvMB3Ff9P0E5nefyfkQeUaoRRcG77WMnAt",
     shortCode: "174379",
     lipaNaMpesaKey : "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     callbackUrl : ""
 }
+module.exports = config
--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,14 @@
 const axios = require('axios')
-const config = require('/Config');
-const { response } = require('express');
+const config = require('./config');
+// const { response } = require('express');
 
 const getAccessToken = async() => {
-    const auth = Buffer.from(${config.consumerKey}: 
-        ${config.consumerSecret}).toString('base64');
+    const auth = Buffer.from(`${config.consumerKey}: 
+        ${config.consumerSecret}`).toString('base64');
 
-    const response = await axios.get('https:                                                                             
+    const response = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
       headers: {
-      '//sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
-      headers: {
-       'Authorization': Basic ${auth},
+       'Authorization': `Basic ${auth}`,
       },
     })
   return response.data.access_token;
